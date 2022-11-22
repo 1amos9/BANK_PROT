1a78cad4c947562d88244546b3dbfd153974673b
amosthibault19@gmail.com <amosthibault19@gmail.com>
2022-11-22 06:52:01 -0500

    #BANK APP

--- script.js
+++ script.js
@@ -222,8 +222,8 @@ btnTransfer.addEventListener('click', function (e) {
     currentAccount.movements.push(-amount);
     receiverAcc.movements.push(amount);
    //Add transfer date
-   currentAccount.movementsDates.push(new Date());
-   receiverAcc.movementsDates.push(new Date());
+   currentAccount.movementsDates.push(new Date().toISOString());
+   receiverAcc.movementsDates.push(new Date().toISOString());
 
     // Update UI
     updateUI(currentAccount);
@@ -239,7 +239,7 @@ btnLoan.addEventListener('click', function (e) {
     // Add movement
     currentAccount.movements.push(amount);
      //Add loan date
-   currentAccount.movementsDates.push(new Date());
+   currentAccount.movementsDates.push(new Date().toISOString());
 
     // Update UI
     updateUI(currentAccount);
